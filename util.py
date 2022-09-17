057c8e817d7c40f843fdc47b7f10f34decee1f34
yashsharma29103 <yashsharma29103@gmail.com>
2022-09-17 19:18:02 +0530

    Final Editing

--- util.py
+++ util.py
@@ -9,10 +9,13 @@ import numpy as np
 from datetime import datetime as dt
 
 def get_estimated_price(date):
+    print(date)
     ordi = dt.strptime(date,"%Y-%m-%d").date().toordinal()
     model=None
-    with open("./artifacts/Final Model.pickle", 'rb') as f:
+    print(ordi)
+    with open("./model/Final Model.pickle", 'rb') as f:
         model = pickle.load(f)
-    pr = model.predict(np.array([ordi]).reshape(-1,1))
+    pr = model.predict([[ordi]])
     flag=0
-    return [pr,flag]     
+    print(pr)
+    return [pr,flag]
